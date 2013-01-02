3975413ccc85f27f51a36933c88ff958e7bfa8da
James Crasta <jcrasta@gmail.com>
2013-01-02 12:17:54 -0700

    Refactor out map parser

--- IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/Game1.cs
+++ IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/Game1.cs
@@ -27,7 +27,7 @@ namespace IndieSpeedRun
 
         //holds all textures after loading for easy access
         public Map currentMap;
-        private Dictionary<String, Texture2D> textures;
+        public Dictionary<String, Texture2D> textures;
         private Player player;
 
         public Game1()
@@ -77,83 +77,9 @@ namespace IndieSpeedRun
             }
 
             //reads in map data from XML
-            readInMapData();
+            MapParser.ReadInMapData(this);
         }
 
-        /// <summary>
-        /// Read in map data from an XML file
-        /// </summary>
-        private void readInMapData()
-        {
-            //Creates XML Reader
-            XmlReader rdr;
-            XmlReaderSettings settings = new XmlReaderSettings();
-
-            //Various settings for the XML reader
-            settings.ConformanceLevel = ConformanceLevel.Fragment;
-            settings.IgnoreWhitespace = true;
-            settings.IgnoreComments = true;
-
-            rdr = XmlReader.Create(@"..\..\..\..\IndieSpeedRunContent\test.xml", settings);
-
-            while (rdr.Read())
-            {
-                //Print out what's being loaded (for debug business)
-                //Console.WriteLine(rdr.LocalName);
-
-                //If nothing is present, go to the first element
-                if (rdr.IsEmptyElement)
-                {
-                    rdr.ReadStartElement();
-                }
-
-                //If it's a map..
-                if (rdr.Name.ToUpper() == "MAP")
-                {
-                    //Skip to name, and set the currentMap's current name to whatever the XML file says it is.
-                    rdr.MoveToAttribute("name");
-                    currentMap.Name = rdr.Value;
-                    //Print it out, for debug purposes.
-                    //Console.WriteLine(currentMap.Name);
-                }
-                else if (rdr.Name.ToUpper() == "BLOCKS")
-                {
-                    //Get the height value of the map
-                    rdr.MoveToAttribute("height");
-                    //..and set the map's height to whatever the file says.
-                    currentMap.Height = Int32.Parse(rdr.Value);
-                    //Keep readin.
-                    rdr.Read();
-
-                    //Run through the file, figure out where the blocks are, and place them where needed.
-                    for (int y = 0; y < currentMap.Height; y++)
-                    {
-                        if (rdr.Name.ToUpper() == "ROW")
-                        {
-                            rdr.Read();
-                        }
-                        for (int x = 0; x < rdr.Value.Length; x++)
-                        {
-                            if (rdr.Value.Substring(x, 1) == "#")
-                            {
-                                currentMap.Blocks.Add(new Block(x * TILE_SIZE, y * TILE_SIZE, new Sprite(textures["tile1"])));
-                            }
-                            /*
-                            else if (rdr.Value.Substring(x, 1) == "P")
-                            {
-                                player = new Player(x * TILE_SIZE, y * TILE_SIZE, new Sprite(textures["HeroBall"]), this);
-                            }*/
-                        }
-                        rdr.Read();
-                        if (rdr.Name.ToUpper() == "ROW")
-                        {
-                            rdr.Read();
-                        }
-                    }
-                    rdr.Read();
-                }
-            }
-        }
 
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
---
+++ IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/MapParser.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace IndieSpeedRun
+{
+    class MapParser
+    {
+        /// <summary>
+        /// Read in map data from an XML file
+        /// </summary>
+        public static void ReadInMapData(Game1 game)
+        {
+            //Creates XML Reader
+            XmlReader rdr;
+            XmlReaderSettings settings = new XmlReaderSettings();
+
+            //Various settings for the XML reader
+            settings.ConformanceLevel = ConformanceLevel.Fragment;
+            settings.IgnoreWhitespace = true;
+            settings.IgnoreComments = true;
+
+            rdr = XmlReader.Create(@"..\..\..\..\IndieSpeedRunContent\test.xml", settings);
+
+            while (rdr.Read())
+            {
+                //Print out what's being loaded (for debug business)
+                //Console.WriteLine(rdr.LocalName);
+
+                //If nothing is present, go to the first element
+                if (rdr.IsEmptyElement)
+                {
+                    rdr.ReadStartElement();
+                }
+
+                //If it's a map..
+                if (rdr.Name.ToUpper() == "MAP")
+                {
+                    //Skip to name, and set the currentMap's current name to whatever the XML file says it is.
+                    rdr.MoveToAttribute("name");
+                    game.currentMap.Name = rdr.Value;
+                    //Print it out, for debug purposes.
+                    //Console.WriteLine(currentMap.Name);
+                }
+                else if (rdr.Name.ToUpper() == "BLOCKS")
+                {
+                    //Get the height value of the map
+                    rdr.MoveToAttribute("height");
+                    //..and set the map's height to whatever the file says.
+                    game.currentMap.Height = Int32.Parse(rdr.Value);
+                    //Keep readin.
+                    rdr.Read();
+
+                    //Run through the file, figure out where the blocks are, and place them where needed.
+                    for (int y = 0; y < game.currentMap.Height; y++)
+                    {
+                        if (rdr.Name.ToUpper() == "ROW")
+                        {
+                            rdr.Read();
+                        }
+                        for (int x = 0; x < rdr.Value.Length; x++)
+                        {
+                            if (rdr.Value.Substring(x, 1) == "#")
+                            {
+                                game.currentMap.Blocks.Add(new Block(x * Game1.TILE_SIZE, y * Game1.TILE_SIZE, new Sprite(game.textures["tile1"])));
+                            }
+                            /*
+                            else if (rdr.Value.Substring(x, 1) == "P")
+                            {
+                                player = new Player(x * TILE_SIZE, y * TILE_SIZE, new Sprite(textures["HeroBall"]), this);
+                            }*/
+                        }
+                        rdr.Read();
+                        if (rdr.Name.ToUpper() == "ROW")
+                        {
+                            rdr.Read();
+                        }
+                    }
+                    rdr.Read();
+                }
+            }
+        }
+
+    }
+}
