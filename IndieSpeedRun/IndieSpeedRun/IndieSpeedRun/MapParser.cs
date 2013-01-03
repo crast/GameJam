0be766495c34406abb20a76f1fc1569ce8bfdea5
Rohit Crasta <rohitcrasta@gmail.com>
2013-01-03 18:17:51 -0500

    merp

--- IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/MapParser.cs
+++ IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/MapParser.cs
@@ -18,7 +18,7 @@ namespace IndieSpeedRun
         }
 
         public static void ReadInMapDataTL(Game1 game) {
-            String filePath = @"..\..\..\..\IndieSpeedRunContent\maps\testmap.json";
+            String filePath = @"..\..\..\..\IndieSpeedRunContent\maps\testmap2.json";
             JObject root = getMapRoot(filePath);
             var tileinfo = parseTileSets((JArray)root["tilesets"]);
             JArray layers = (JArray)root["layers"];
