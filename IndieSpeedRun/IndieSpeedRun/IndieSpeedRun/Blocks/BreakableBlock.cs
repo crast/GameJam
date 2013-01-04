8b7c9eda8cd96345863603e1d5c1cfeb7fa3149f
Rohit Crasta <rohitcrasta@gmail.com>
2013-01-03 23:07:07 -0500

    merp

---
+++ IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/Blocks/BreakableBlock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IndieSpeedRun.Blocks
+{
+    public class BreakableBlock : IndieSpeedRun.Block
+    {
+        public BreakableBlock(int x, int y, int width, int height)
+            :base(x,y, width, height)
+        {
+
+            //this.Kill(); // Do not draw me
+        }
+
+        public void Destroy()
+        {
+            this.Kill(); //stop drawing
+            //stop all collision
+        }
+    }
+}
