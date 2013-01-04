ae07c6a3fbee3f114194271d2eac6b9ad4aa0352
crast <contact@jamescrasta.com>
2013-01-04 01:47:31 -0700

    Add cool/warm frames

--- IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/Hud.cs
+++ IndieSpeedRun/IndieSpeedRun/IndieSpeedRun/Hud.cs
@@ -10,22 +10,37 @@ namespace IndieSpeedRun
     class Hud
     {
         private Player player;
+        private Rectangle screenBox;
         private Texture2D thermoFrame;
         private Texture2D thermoFill;
+        private Texture2D warningCool;
+        private Texture2D warningHot;
 
         public Hud(Game1 game, Player player) {
+            this.screenBox = new Rectangle(0, 0, game.mapWidth, game.mapHeight);
             this.player = player;
             this.thermoFrame = game.ConditionalLoadSprite("thermoframe", "sprites/thermo_frame-01");
             this.thermoFill = game.ConditionalLoadSprite("thermofill", "sprites/thermo_fill-01");
-
+            this.warningCool = game.ConditionalLoadSprite("tiles/warning_cool-01");
+            this.warningHot = game.ConditionalLoadSprite("tiles/warning_heat-01");
         }
 
         public void Draw(SpriteBatch batch)
         {
+            // Cool warning frame
+            if (player.Heat < 10)
+                batch.Draw(warningCool, screenBox, Color.White);
+            
+            // Hot warning frame
+            if (player.Heat > 90)
+                batch.Draw(warningHot, screenBox, Color.White);
+            
+
+            // Thermo bar frame
             Rectangle frameBox = new Rectangle(0, 0, 40, 264);
             batch.Draw(thermoFrame, frameBox, frameBox, Color.White);
 
-
+            // thermo bar
             int height = (int)(player.Heat * 248 / 100);
             Rectangle fillBox = new Rectangle(8, (248 - height) + 8, 24, height);
             Rectangle fillSourceBox = new Rectangle(0, 248 - height, 24, height);
